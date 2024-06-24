50b23a2d41a763108ac0ebd9b0dd15835ebf89ec
Stepan <pirogov1923@mail.ru>
2024-06-24 21:16:55 +0500

    visual fix (again)

--- scripts/App/app.js
+++ scripts/App/app.js
@@ -75,9 +75,9 @@ export class App {
     forms.forEach((form) => {
       figureImg.src = `./images/${form.figure.name}.svg`;
       figureImg.alt = form.figure.name;
-      currentFigures += `${form.side.toUpperCase()}: ${figureImg.outerHTML} `;
+      currentFigures += `${form.side.toUpperCase()}:${figureImg.outerHTML}`;
     });
-    figures.innerHTML = currentFigures.substring(0, currentFigures.length - 1);
+    figures.innerHTML = currentFigures;
     this.solutionStep.append(figures);
   }
 
